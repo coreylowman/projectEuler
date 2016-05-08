116eb1f9d67b30eda8da1cf7901176f971cd6bbe
Corey Lowman <coreylowman@github.com>
2016-05-08 14:01:15 -0400

    Fixing bug in 45

--- src/problems/problem045.rs
+++ src/problems/problem045.rs
@@ -1,20 +1,15 @@
 extern crate num;
 
-fn half(n: u64) -> u64 {
-    n / 2
-}
-
 fn t(n: u64) -> u64 {
-    half(n) * (n + 1u64)
+    n * (n + 1) / 2
 }
 
 fn p(n: u64) -> u64 {
-    let half = half(n);
-    3u64 * n * half - half
+    n * (3 * n - 1) / 2
 }
 
 fn h(n: u64) -> u64 {
-    n * (2u64 * n - 1u64)
+    n * (2 * n - 1)
 }
 
 fn go() -> String {
@@ -33,6 +28,7 @@ fn go() -> String {
             return ts[i].to_string();
         }
     }
+
     0.to_string()
 }
 
